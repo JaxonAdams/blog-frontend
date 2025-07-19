c53799b6b07507810ad7df4541c34c0364aff230
Jaxon Adams <jadamsresume1@gmail.com>
2025-07-19 11:23:18 -0600

    Fix navbar text color

--- src/app/components/navbar/core.cljs
+++ src/app/components/navbar/core.cljs
@@ -4,7 +4,7 @@
   (fn []
     [:nav.navbar.navbar-expand-lg.navbar-dark.border-bottom.card.mb-4
      [:div.container
-      [:a.navbar-brand {:href "#"} "Jaxon Adams"]
+      [:a.navbar-brand.text-light {:href "#"} "Jaxon Adams"]
       [:button.navbar-toggler {:type "button" :data-bs-toggle "collapse" :data-bs-target "#navbarNav"}
        [:span.navbar-toggler-icon]]
       [:div#navbarNav.collapse.navbar-collapse
